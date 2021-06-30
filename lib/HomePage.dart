402141a3d1030d82013b63c2daa624c6adec7e1c
Your Name <you@example.com>
2021-07-01 02:29:37 +0530

    resolving conflicts

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,64 +1,185 @@
-import 'package:drusti/LoginAndRegistration.dart';
-import 'package:drusti/main.dart';
-import 'package:flutter/cupertino.dart';
+// @dart=2.9
+import 'package:drusti/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:drusti/LoginAndRegistration.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget{
-  HomePage();
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferences.getInstance().then((prefs) {
+    runApp(MyApp(prefs: prefs));
+  });
+}
 
+class MyApp extends StatelessWidget {
+  SharedPreferences prefs;
+  MyApp({ this.prefs});
   @override
   Widget build(BuildContext context) {
-     return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp(
+      title: 'Drusti',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: Home(),
+        primarySwatch: Colors.amber,),
+      home: _decideLandingPage(),
     );
   }
-
+  _decideLandingPage(){
+    if(this.prefs == null || this.prefs.getString("token") == null){
+      return LandingPage(title:"Choose User");
+    }else{
+      return HomePage();
+    }
+  }
 }
 
-class Home extends StatefulWidget{
+class LandingPage extends StatefulWidget {
+  LandingPage({this.title}) : super();
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  HomeState createState() =>
-    HomeState();
-
+  LandingPageState createState() => LandingPageState();
 }
 
-class HomeState extends State{
- 
+class LandingPageState extends State<LandingPage> {
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text('home here'),
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: Container(
-        child: TextButton(onPressed: () { 
-          logOut();
-        },
-        child: Text("LogOut"),
-          
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+            const SizedBox(height: 30),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.blue,
+                      ),
+                      width: 100,
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      primary: Colors.white,
+                      textStyle: const TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      Navigator.push<void>(
+                        context,
+                        MaterialPageRoute<void>(
+                          builder: (BuildContext context) =>
+                              LoginAndRegisteration(
+                                type: 'Institute',
+                              ),
+                        ),
+                      );
+                    },
+                    child: const Text('Institution'),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30, width: 100),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.blue,
+                      ),
+                      width: 100,
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      primary: Colors.white,
+                      textStyle: const TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Vechicle'),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+              width: 100,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      primary: Colors.white,
+                      textStyle: const TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Parent'),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-      ),
-      
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-  Future<void> logOut() async {
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    pref.clear();
-    pref.commit();
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-          builder: (context) => MyApp()),
-    );
-  
-  }
 
-  
 }
